e9cc99acfdb905fbea97f60f2c5d441b59ea5d97
Lan Kim Ho <lan.ho@asnet.com.vn>
2024-06-18 11:46:05 +0700

    feature/implement-Checkbox-component

---
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+// Components
+import Checkbox from './index';
+
+const meta = {
+  title: 'Components/Checkbox',
+  component: Checkbox,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    disabled: {
+      control: 'boolean',
+    },
+  },
+  args: {
+    onChange: fn(),
+  },
+} satisfies Meta<typeof Checkbox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    id: 'option1',
+    label: 'Option 1',
+    description:
+      'guidelines regarding color contrast ratios and provide options for users to adjust text size',
+  },
+};
+
+export const IsDisabledCheckbox: Story = {
+  args: {
+    label: 'Option 2',
+    description:
+      'guidelines regarding color contrast ratios and provide options for users to adjust text size',
+    disabled: true,
+    checked: true,
+  },
+};
---
+++ src/components/Checkbox/Checkbox.test.tsx
@@ -0,0 +1,26 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+
+// Components
+import Checkbox from './index';
+
+describe('Checkbox component', () => {
+  test('renders the Checkbox with label and description', () => {
+    render(<Checkbox label="Option 1" description="Description 1" />);
+    expect(screen.getByText('Option 1')).toBeInTheDocument();
+    expect(screen.getByText('Description 1')).toBeInTheDocument();
+  });
+
+  test('handle onChange event when click on checkbox', () => {
+    const handleCheck = jest.fn();
+    render(<Checkbox label="Option 1" id="option1" onChange={handleCheck} />);
+    fireEvent.click(screen.getByRole('checkbox'));
+    expect(handleCheck).toHaveBeenCalledTimes(1);
+  });
+
+  test('handle onChange event when click on label', () => {
+    const handleCheck = jest.fn();
+    render(<Checkbox label="Option 1" id="option1" onChange={handleCheck} />);
+    fireEvent.click(screen.getByText('Option 1'));
+    expect(handleCheck).toHaveBeenCalledTimes(1);
+  });
+});
