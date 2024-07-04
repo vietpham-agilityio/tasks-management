2ab68543c203e0d4a0929eca36f2be223f108aa1
Lan Kim Ho <lan.ho@asnet.com.vn>
2024-07-04 15:25:07 +0700

    refactor/update-layout-for-app

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,9 +1,31 @@
 import { ReactNode } from 'react';
+import type { Metadata } from 'next';
 
-const Layout = ({
+// Components
+import { PublicNavBar } from '@/components';
+
+export const metadata: Metadata = {
+  title: 'Task Management',
+  description: 'Application to manage task built by NextJS',
+  icons: [
+    {
+      rel: 'icon',
+      url: '/favicon.ico',
+    },
+  ],
+};
+
+const PublicLayout = ({
   children,
 }: Readonly<{
   children: ReactNode;
-}>) => <div className="h-full">{children}</div>;
+}>) => (
+  <div className="grid grid-flow-row md:grid-flow-col md:gap-8">
+    <div className="row-auto md:col-span-1">
+      <PublicNavBar />
+    </div>
+    <div className="row-auto md:col-span-11">{children}</div>
+  </div>
+);
 
-export default Layout;
+export default PublicLayout;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,7 @@ import type { Metadata } from 'next';
 import { Manrope } from 'next/font/google';
 import './globals.css';
 
-// Auth
-import { auth } from '@/auth';
-
-// Components
-import { AdminNavBar, PublicNavBar } from '../components';
-
-// Utlils
+// Utils
 import { cn } from '@/utils';
 
 const manRope = Manrope({ subsets: ['latin'] });
@@ -30,22 +24,10 @@ export default async function RootLayout({
 }: Readonly<{
   children: ReactNode;
 }>) {
-  const session = await auth();
-  const isAdmin = !!session;
-
   return (
     <html lang="en">
       <body className={cn(manRope.className, 'bg-zinc-50 px-6 my-[21px]')}>
-        <div className="flex flex-col md:flex-row md:gap-8">
-          <div className="w-full md:max-w-[200px]">
-            {isAdmin ? <AdminNavBar /> : <PublicNavBar />}
-          </div>
-          {/* Height - Small Screen = 100vh - 21*2 + 48 ((padding-x) * 2 + navbar ) 
-            Height - Big Screen = 100vh - (padding-x) * 2  */}
-          <div className="min-w-0 w-full h-[calc(100vh_-_90px)] lg:h-[calc(100vh_-_42px)]">
-            {children}
-          </div>
-        </div>
+        {children}
       </body>
     </html>
   );
