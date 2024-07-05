5dc86e3776c3a4ef7f621a41047959d36471a232
Ngoc Quang Nguyen <ngoc.nguyenquang@asnet.com.vn>
2024-07-05 14:24:12 +0700

    Feat(#62): implement query database function

--- src/api/project.ts
+++ src/api/project.ts
@@ -1,18 +1,18 @@
 import { unstable_cache as cache } from 'next/cache';
 
 // DB
-import { getDataFromFirestore } from '@/db';
+import { getDocuments } from '@/db';
 
 // Constants
-import { COLLECTION, LIMIT_ITEMS, TAGS } from '@/constants';
+import { COLLECTION, TAGS } from '@/constants';
 
 // Types
 import { Project } from '@/models';
 
 export const getProjects = async () => {
-  const projects = await cache(getDataFromFirestore, [TAGS.PROJECT_LIST], {
+  const projects = await cache(getDocuments, [TAGS.PROJECT_LIST], {
     tags: [TAGS.PROJECT_LIST],
-  })<Project>(COLLECTION.PROJECTS, LIMIT_ITEMS);
+  })<Project>(COLLECTION.PROJECTS, { field: 'updatedAt', type: 'desc' }, 10);
 
   return projects;
 };
