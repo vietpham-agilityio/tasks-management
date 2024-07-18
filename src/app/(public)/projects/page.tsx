beeef3362363c576bd44365f830747acb29ebdf7
Ngoc Quang Nguyen <ngoc.nguyenquang@asnet.com.vn>
2024-07-18 16:25:49 +0700

    Feat(#115): Update dark theme public project list page

--- src/app/(public)/projects/page.tsx
+++ src/app/(public)/projects/page.tsx
@@ -43,7 +43,7 @@ const ProjectListPage = async ({
   if (error) return <ErrorMessage message={error} />;
 
   return (
-    <main className="bg-white dark:bg-neutral-900 p-4 h-full">
+    <main className="p-4 h-full">
       <div className="flex flex-row justify-between items-center py-8 ">
         <div className=" dark:text-white ">
           <h1 className="font-bold text-3xl">Projects</h1>
